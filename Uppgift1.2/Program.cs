d16e549c6d095d467b82d2a739db68e287e63a35
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:19:50 +0200

    Add project files.

---
+++ Uppgift1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift_1._2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("VIRUS");
+            Console.ReadKey();
+           
+        }
+    }
+}
